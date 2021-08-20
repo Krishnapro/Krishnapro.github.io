6bec34e0638528d925f1f0cecb643ac6c790e801
Krishna Kumar <krishnachaurasia1998@gmail.com>
2021-08-20 19:18:38 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// ========= menu show y Hidden =======
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+    /* ====== Menu Show ===== */
+    /* validation if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+      /* ====== Menu Hidden ===== */
+    /* validation if constant exists */
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
+        })
+    }

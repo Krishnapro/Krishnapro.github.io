1c265da52d90023fcfbcc7fcee89bfd2f298f627
Krishna <krishnachaurasia1998@gmail.com>
2021-08-21 16:59:44 +0530

    update work

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,12 @@ if(navToggle){
             navMenu.classList.remove('show-menu')
         })
     }
+    /* ================ remove menu mobile ============= */
+    const navLink = document.querySelectorAll('.nav_link')
+    function linkAction(){
+        const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav_link, we remove show-menu class
+        navMenu.classList.remove('show-menu')
+
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction))
